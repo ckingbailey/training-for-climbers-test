6ceb94a5a5e2ce462e76a53eef6311cb5347633e
ckb <ckingbailey@gmail.com>
2021-02-28 17:00:40 -0800

    transform no work

--- processQuestions.js
+++ processQuestions.js
@@ -1,27 +1,42 @@
 const fs = require('fs');
-const { Transform } = require('stream');
+const readline = require('readline');
+const thru = require('through2');
 
 function transformQuestionsIntoHTML() {
-    reader = fs.createReadStream(__dirname + '/questions.yml');
+    reader = fs.createReadStream(__dirname + '/questions.yml', { encoding: 'utf8' });
     writer = fs.createWriteStream(__dirname + '/questions.html');
+    lineReader = readline.createInterface({ input: reader });
 
-    questionTemplate = fs.readFileSync(__dirname + '/questionTemplate.html')
-    tranformQuestion = createQuestionTranformer(questionTemplate)
-    transformer = new Transform
-    transformer._transform = (d, enc, callback) => {
-
-    }
+    questionTemplate = fs.readFileSync(__dirname + '/questionTemplate.html');
+    transformQuestion = createQuestionTranformer(questionTemplate);
     
-    reader.pipe(transform).pipe(process.stdout).on('end', () => console.log('fin!'));
+    (lineReader
+        .on('data', d => {
+            console.log(typeof d);
+            console.log(d.length);
+        })
+        .pipe(thru({ objectMode: false }, transformQuestion))
+        .pipe(process.stdout)
+        .on('end', () => console.log('fin!'))
+    );
 }
+
+// function whatIsIt(d, enc)
 
 function createQuestionTranformer(template) {
     return function(d, enc, callback) {
+        console.log('[INFO] `enc` data is a ' + enc);
+        console.log('[INFO] d is a typeof ' + typeof d);
+        console.log('[INFO] d is ' + d.length + ' long');
+        console.log('[INFO] d has props ' + Object.getOwnPropertyNames(d));
+
         questionSlug = d.toLowerCase().replace(/\s+/g, '-');
         questionElement = (template
             .replace('{% questionText %}', d)
-            .replace(/{% questionSlug %}/g, questionSlug))
-        return questionElement
+            .replace(/{% questionSlug %}/g, questionSlug));
+        this.push(questionElement);
+        
+        callback();
     }
 }
 
